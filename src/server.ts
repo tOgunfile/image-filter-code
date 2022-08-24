165b36821da1d8e5078debbadceff7b2504330b4
tOgunfile <tosinogunfile@gmail.com>
2022-08-24 02:14:45 +0100

    second commit

--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,39 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  app.get("/filteredimage", (req: Request, res: Response) => {
-      let { URL } = req.query.image_url;
-      // IT SHOULD
-      //    1
-      //    1. validate the image_url query
-      if (!URL) {
-        return res.status(400)
-          .send(`Insert image url`);
-      };
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    let { image_url } = req.query;
+    image_url = image_url.toString();
+    // IT SHOULD
+    //    1
+    //    1. validate the image_url query
+    if (!image_url) {
+      return res.status(400).send("Insert image url");
+    }
 
-      //    2. call filterImageFromURL(image_url) to filter the image
-      image = filterImageFromURL(URL);
+    //    2. call filterImageFromURL(image_url) to filter the image
+    let filteredImage = await filterImageFromURL(image_url);
 
-      //    3. send the resulting file in the response
-      return res.status(200)
-        .sendFile(image);
-
+    //    3. send the resulting file in the response
+    res.status(200).sendFile(filteredImage, () => {
       //    4. deletes any files on the server on finish of the response
-      image = deleteLocalFiles(image);
+      deleteLocalFiles([filteredImage]);
     });
+  });
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
@@ -44,17 +42,16 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
